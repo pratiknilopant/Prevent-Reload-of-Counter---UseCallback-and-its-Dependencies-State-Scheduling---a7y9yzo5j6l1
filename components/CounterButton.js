92d3399a5bfcc8bb56aeab0038dc740f7bcb5eea
pratiknilopant <mr.pratiknilopant@gmail.com>
2023-11-01 18:49:16 +0530

    Final Commit

--- components/CounterButton.js
+++ components/CounterButton.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
-function CounterButton() {
+function CounterButton({ increment }) {
+     const incrementAction = useCallback(() => {
+          increment();
+     }, [increment]);
+     
      /*DO NOT EDIT THIS LINE */ console.log("CounterButton rendered!"); //DO NOT edit this line 
+
+     
+
+     return (
+          <button onClick={increment}>
+               Increment
+          </button>
+     );
 
 }
 
-export default /*___________*/
+export default CounterButton;
